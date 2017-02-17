66dbe0b359ad62069c701cc1895bd7c431ba1ce4
Hidekazu Ikeno <hide.ikeno@gmail.com>
2017-02-17 09:04:59 +0900

    rename config files

---
+++ .zsh/20_modules.zsh
@@ -0,0 +1,16 @@
+autoload -Uz cdr
+
+autoload -Uz history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+
+autoload -Uz modify-current-argument
+autoload -Uz smart-insert-last-word
+autoload -Uz terminfo
+autoload -Uz vcs_info
+autoload -Uz zcalc
+autoload -Uz zmv
+autoload -Uz run-help-git
+autoload -Uz run-help-svk
+autoload -Uz run-help-svn
+
