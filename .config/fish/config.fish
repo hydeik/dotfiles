66ff56f0496aa72bfeb599c08fc71a434d41408c
Hidekazu Ikeno <hide.ikeno@gmail.com>
2017-02-21 20:52:12 +0900

    Call keychain at login

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,3 +16,11 @@ source ~/.config/fish/env.fish
 if status --is-interactive
     source ~/.config/fish/aliases.fish
 end
+
+# SSH keychain
+if status --is-login
+   if type -q keychain
+       keychain --nogui --quiet ~/.ssh/id_ecdsa ~/.ssh/id_github_ecdsa
+       source ~/.keychain/(hostname)-fish 
+   end
+end
