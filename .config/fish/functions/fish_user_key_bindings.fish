2b42e3662a6a82dc893a0934e52b85d2dc04bd8d
Hidekazu Ikeno <hide.ikeno@gmail.com>
2017-04-13 13:32:06 +0900

    Use fisherman plugin manager

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,29 +1,39 @@
-function fish_user_key_bindings -d 'define my bindings'
-    #
-    # Rebind fzf functions
-    #
-    # Ctrl-t: fuzzy search for files
-    # Ctrl-r: fuzzy search for history
-    # Alt-x: does the reverse isearch and execute command immediately
-    # Alt-d: cd into sub-directories (recursively searched)
-    # Alt-D: cd into sub-directories, including hidden ones.
-    #
-    bind \ct '__fzf_find_file'
-    bind \cr '__fzf_reverse_isearch'
-    bind \ex '__fzf_find_and_execute'
-    bind \ed '__fzf_cd'
-    bind \eD '__fzf_cd_with_hidden'
-  
-    if bind -M insert >/dev/null ^/dev/null
-        bind -M insert \cf '__fzf_find_file'
-        bind -M insert \cr '__fzf_reverse_isearch'
-        bind -M insert \ex '__fzf_find_and_execute'
-        bind -M insert \ed '__fzf_cd'
-        bind -M insert \eD '__fzf_cd_with_hidden'
+function fish_user_key_bindings
+    ### fzf ###
+    set -q FZF_LEGACY_KEYBINDINGS
+    or set -l FZF_LEGACY_KEYBINDINGS 1
+    if test "$FZF_LEGACY_KEYBINDINGS" -eq 1
+        bind \ct '__fzf_find_file'
+        bind \cr '__fzf_reverse_isearch'
+        bind \cx '__fzf_find_and_execute'
+        bind \ec '__fzf_cd'
+        bind \eC '__fzf_cd_with_hidden'
+        if bind -M insert >/dev/null ^/dev/null
+            bind -M insert \ct '__fzf_find_file'
+            bind -M insert \cr '__fzf_reverse_isearch'
+            bind -M insert \cx '__fzf_find_and_execute'
+            bind -M insert \ec '__fzf_cd'
+            bind -M insert \eC '__fzf_cd_with_hidden'
+        end
+    else
+        bind \cf '__fzf_find_file'
+        bind \cr '__fzf_reverse_isearch'
+        bind \ex '__fzf_find_and_execute'
+        bind \ed '__fzf_cd'
+        bind \eD '__fzf_cd_with_hidden'
+        if bind -M insert >/dev/null ^/dev/null
+            bind -M insert \cf '__fzf_find_file'
+            bind -M insert \cr '__fzf_reverse_isearch'
+            bind -M insert \ex '__fzf_find_and_execute'
+            bind -M insert \ed '__fzf_cd'
+            bind -M insert \eD '__fzf_cd_with_hidden'
+        end
     end
-
-    #
-    # Ctrl-g repository search by ghq (require fzf, peco or percol for the selector)
-    #
-    bind \cg __ghq_crtl_g
+    ### fzf ###
+    ### ghq ###
+    bind \cg '__ghq_crtl_g'
+    if bind -M insert >/dev/null ^/dev/null
+        bind -M insert \cg '__ghq_crtl_g'
+    end
+    ### ghq ###
 end
