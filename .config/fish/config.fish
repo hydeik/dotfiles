a107579030ab2a8306f451703f39ffb1388e5fc9
Hidekazu Ikeno <hide.ikeno@gmail.com>
2017-04-13 13:21:03 +0900

    Remove fundle

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,3 @@
-# fundle -- plugin manager
-fundle plugin 'edc/bass'
-fundle plugin 'decors/fish-ghq'
-fundle plugin 'd42/fish-pip-completion'
-fundle plugin 'fisherman/fzf'
-fundle plugin 'fisherman/termcolours'
-fundle plugin 'oh-my-fish/theme-bobthefish'
-#fundle plugin 'nesl247/fish-theme-dracula'
-
-fundle init
-
 # Environment variables
 source ~/.config/fish/env.fish
 
