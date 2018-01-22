10f19e5b4bafe5e4ef3b7a3ccb0698ce62ddb2c6
Hidekazu Ikeno <hide.ikeno@gmail.com>
2018-01-23 02:04:51 +0900

    Add gitconfig and shell script for automatic link

---
+++ mklink.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CWD=`pwd`
+DOTFILES='gitconfig latexmk tmux.conf vimrc zshenv'
+
+if [ -z "${XDG_CONFIG_HOME}" ]
+then
+    XDG_CONFIG_HOME="${HOME}/.config"
+fi
+
+for f in ${DOTFILES}
+do
+    ln -fsv ${CWD}/${f} ${HOME}/.${f}
+done
+
+for dir in ${CWD}/config/*
+do
+    ln -fsv ${dir} ${XDG_CONFIG_HOME}/${dir##*/}
+done
