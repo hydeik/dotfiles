d73cb1e6b0d93cfff047892d093905bcd19362d7
Hidekazu Ikeno <hide.ikeno@gmail.com>
2018-05-08 17:21:00 +0900

    tmux: configure for true color

--- mklink.sh
+++ mklink.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CWD=`pwd`
-DOTFILES='editorconfig gitconfig latexmkrc tmux.conf zshenv'
+CWD=$(pwd)
+DOTFILES='editorconfig gitconfig latexmkrc zshenv'
 
 if [ -z "${XDG_CONFIG_HOME}" ]
 then
@@ -10,26 +10,30 @@ fi
 
 for f in ${DOTFILES}
 do
-    ln -fsv ${CWD}/${f} ${HOME}/.${f}
+    ln -fsv "${CWD}/${f}" "${HOME}/.${f}"
 done
 
 for dir in ${CWD}/config/{fish,zsh}
 do
-    ln -fnsv ${dir} ${XDG_CONFIG_HOME}/${dir##*/}
+    ln -fnsv "${dir}" "${XDG_CONFIG_HOME}/${dir##*/}"
 done
 
 # Make symlink for VIM configuration
-ln -fnsv ${CWD}/vim $HOME/.vim
+ln -fnsv "${CWD}/vim" "${HOME}/.vim"
 
 # Make symlink for NeoVim configuration
-ln -fnsv ${CWD}/vim ${XDG_CONFIG_HOME}/nvim
+ln -fnsv "${CWD}/vim" "${XDG_CONFIG_HOME}/nvim"
+
+# Make symling for Tmux configuration
+ln -fnsv "${CWD}/tmux" "${HOME}/.tmux"
+ln -fsv  "${CWD}/tmux.conf" "${HOME}/.tmux.conf"
 
 if [ "$(uname)" == 'Darwin' ]
 then
-    ln -fnsv ${CWD}/hammerspoon ${HOME}/.hammerspoon
+    ln -fnsv "${CWD}/hammerspoon" "${HOME}/.hammerspoon"
 
     for f in ${CWD}/config/karabiner/assets/complex_modifications/*.json
     do
-        ln -fsv $f ${XDG_CONFIG_HOME}/karabiner/assets/complex_modifications/
+        ln -fsv "$f" "${XDG_CONFIG_HOME}/karabiner/assets/complex_modifications/"
     done
 fi
