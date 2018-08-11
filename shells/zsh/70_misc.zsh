892910c74c7bcc14bb8ec61e7a7596b3fbb3e62b
Hidekazu Ikeno <hide.ikeno@gmail.com>
2018-08-11 21:58:41 +0900

    zsh: pyenv init also setup completion.

--- shells/zsh/70_misc.zsh
+++ shells/zsh/70_misc.zsh
@@ -31,24 +31,6 @@ if (( ${+commands[pip]} )); then
     fi
 fi
 
-# pyenv
-if (( ${+commands[pyenv]} )); then
-    compctl -K _pyenv pyenv
-
-    _pyenv() {
-        local words completions
-        read -cA words
-
-        if [ "${#words}" -eq 2 ]; then
-          completions="$(pyenv commands)"
-        else
-          completions="$(pyenv completions ${words[2,-2]})"
-        fi
-
-        reply=(${(ps:\n:)completions})
-    }
-fi
-
 # pipenv
 if (( ${+commands[pipenv]} )); then
     #compdef pipenv
