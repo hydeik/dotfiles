9ede25e3e1b9f5ea55298a7bd1dbbb9f8c66ca09
Hidekazu Ikeno <hide.ikeno@gmail.com>
2021-10-18 02:40:42 +0900

    nvim: update statusline

--- editors/nvim/lua/rc/statusline.lua
+++ editors/nvim/lua/rc/statusline.lua
@@ -2,7 +2,7 @@ local M = {}
 
 -- [[ Components ]]
 
--- File info
+-- [ File info ]
 local function file_readonly()
   return vim.bo.readonly and " " or ""
 end
@@ -112,6 +112,36 @@ local function progress()
   return [[%3P]]
 end
 
+-- [ Git info ]
+
+local function git_branch()
+  local branch = vim.b.gitsigns_head or vim.g.gitsigns_head or ""
+  if #branch > 0 then
+    return string.format(" %s", branch)
+  end
+  return ""
+end
+
+local function git_diff(type, icon)
+  local d = vim.b.gitsigns_status_dict
+  if d and d[type] and d[type] > 0 then
+    return string.format("%s%d", icon, d[type])
+  end
+  return ""
+end
+
+local function git_diff_added()
+  return git_diff("added", " ")
+end
+
+local function git_diff_removed()
+  return git_diff("removed", " ")
+end
+
+local function git_diff_changed()
+  return git_diff("changed", " ")
+end
+
 -- -- Vi mode
 -- local current_mode_label = setmetatable({
 --   ["n"] = "NORMAL",
@@ -165,341 +195,50 @@ end
 --   return string.format("%s%%#Statusline%s#%s%s", left_separator(hl), hl, label, right_separator(hl))
 -- end
 
--- -- File info
--- local function file_readonly(inactive)
---   if vim.bo.filetype == "help" then
---     return ""
---   end
---   local hl = inactive and "Inactive" or "FileReadonly"
---   if vim.bo.readonly then
---     return string.format("%%#StatusLine%s# ", hl)
---   else
---     return ""
---   end
--- end
+-- [ LSP status ]
+local function lsp_client_names()
+  local clients = {}
+  for _, client in pairs(vim.lsp.buf_get_clients(0)) do
+    clients[#clients + 1] = client.name
+  end
+  if #clients > 0 then
+    return "  " .. table.concat(clients, "|")
+  end
+  return ""
+end
 
--- local function file_modified(inactive)
---   if vim.tbl_contains({ "help", "denite", "fzf", "tagbar" }, vim.bo.filetype) then
---     return ""
---   end
---   local hl = inactive and "Inactive" or "FileModified"
---   if vim.bo.modifiable then
---     if vim.bo.modified then
---       return string.format("%%#StatusLine%s#  ", hl)
---     end
---   end
---   return ""
--- end
+local function get_diagnostics_count(severity)
+  local active_clients = vim.lsp.buf_get_clients(0)
+  if not active_clients then
+    return 0
+  end
+  local count = 0
+  for _, client in pairs(active_clients) do
+    count = count + vim.lsp.diagnostic.get_count(0, severity, client.id)
+  end
+  return count
+end
 
--- local function file_icon(inactive)
---   local hl = inactive and "Inactive" or "FileIcon"
---   local ft = vim.bo.filetype
---   if #ft > 0 then
---     local ok, devicons = pcall(require, "nvim-web-devicons")
---     if ok then
---       local f_name = vim.fn.expand "%:t"
---       local f_ext = vim.fn.expand "%:e"
---       local icon = devicons.get_icon(f_name, f_ext)
---       if icon ~= nil then
---         return string.format("%%#StatusLine%s#%s", hl, icon)
---       elseif ft == "help" then
---         return string.format("%%#StatusLine%s#", hl)
---       end
---     end
---   end
---   return ""
--- end
+local function get_diagnostics(severity, icon)
+  local count = get_diagnostics_count(severity)
+  return count ~= 0 and string.format("%s%d", icon, count) or ""
+end
 
--- local function file_size(inactive)
---   local file = vim.fn.expand "%:p"
---   if string.len(file) == 0 then
---     return ""
---   end
---   -- Format file size
---   local hl = inactive and "Inactive" or "FileSize"
---   local size = vim.fn.getfsize(file)
---   if size == 0 or size == -1 or size == -2 then
---     return ""
---   end
---   local kb = 1024
---   local mb = 1024 * 1024
---   local gb = 1024 * 1024 * 1024
---   if size < kb then
---     size = size .. "b "
---   elseif size < mb then
---     size = string.format("%.1f%s", size / kb, "k ")
---   elseif size < gb then
---     size = string.format("%.1f%s", size / mb, "m ")
---   else
---     size = string.format("%.1f%s", size / gb, "g ")
---   end
---   return string.format("%%#StatusLine%s# %s", hl, size)
--- end
+local function diagnostics_errors()
+  return get_diagnostics("Error", " ")
+end
 
--- local function file_name(inactive)
---   local ft_lean = { "tagbar", "vista", "defx", "coc-explorer", "magit" }
---   local ft = vim.bo.filetype
---   if vim.tbl_contains(ft_lean, ft) then
---     return " "
---   end
+local function diagnostics_warnings()
+  return get_diagnostics("Warning", " ")
+end
 
---   local hl = inactive and "Inactive" or "FileName"
---   local name = vim.api.nvim_buf_get_name(0) -- full path
---   if name == "" then
---     name = "[No Name]"
---   else
---     name = vim.fn.fnamemodify(name, ":.") -- path relative to current directory
---     if #name > 24 then
---       name = vim.fn.pathshorten(name)
---     end
---   end
+local function diagnostics_info()
+  return get_diagnostics("Warning", " ")
+end
 
---   return string.format("%%#StatusLine%s# %s ", hl, name)
--- end
-
--- local function file_info(inactive)
---   local hl_left = inactive and "Inactive" or "FileIcon"
---   local hl_right = inactive and "Inactive" or "FileName"
-
---   local s1 = left_separator(hl_left)
---   s1 = s1 .. file_icon(inactive)
---   s1 = s1 .. file_name(inactive)
---   local s2 = file_modified(inactive)
---   s2 = s2 .. file_readonly(inactive)
---   s2 = s2 .. file_size(inactive)
---   if #s2 > 0 and not inactive then
---     hl_right = "FileSize"
---   end
---   s2 = s2 .. right_separator(hl_right)
---   return s1 .. s2
--- end
-
--- -- file encoding & format
--- local function file_encoding(inactive)
---   local fenc = vim.bo.fileencoding
---   if fenc == "" then
---     fenc = vim.o.encoding
---   end
---   local hl = inactive and "Inactive" or "FileEncoding"
---   return string.format("%%#StatusLine%s# %s", hl, fenc)
--- end
-
--- local function file_format(inactive)
---   local ff = vim.bo.fileformat
---   local hl = inactive and "Inactive" or "FileFormat"
---   local icon
---   if ff == "mac" then
---     icon = " "
---   elseif ff == "unix" then
---     icon = " "
---   elseif ff == "dos" then
---     icon = " "
---   end
---   return string.format("%%#StatusLine%s# %s", hl, icon)
--- end
-
--- local function file_encoding_and_format(inactive)
---   local hl_left = inactive and "Inactive" or "FileEncoding"
---   local hl_right = inactive and "Inactive" or "FileFormat"
---   local s = left_separator(hl_left)
---   s = s .. file_encoding(inactive)
---   s = s .. file_format(inactive)
---   s = s .. right_separator(hl_right)
---   return s
--- end
-
--- -- Lines/columns
--- local function line_colmun_info(inactive)
---   local hl = inactive and "Inactive" or "LineColumn"
---   return string.format("%s%%#StatusLine%s#  %%3l/%%L  %%-2v%s", left_separator(hl), hl, right_separator(hl))
--- end
-
--- -- VCS
--- local function git_info(inactive)
---   if not inactive then
---     local icon = {
---       branch = " ",
---       diff_added = " ",
---       diff_removed = " ",
---       diff_modified = " ",
---     }
---     local ok, data = pcall(vim.api.nvim_buf_get_var, 0, "gitsigns_status_dict")
---     if ok then
---       local s = "%#StatusLineBranchIconSep#" .. separator.left
---       s = s .. "%#StatusLineBranchIcon#" .. icon.branch
---       s = s .. "%#StatusLineBranchName#" .. data.head
---       local hl_right = "BranchName"
---       if data.added ~= nil and data.added > 0 then
---         s = s .. " %#StatusLineDiffAdded#" .. icon.diff_added .. data.added
---         hl_right = "DiffAdded"
---       end
---       if data.changed ~= nil and data.changed > 0 then
---         s = s .. " %#StatusLineDiffModified#" .. icon.diff_modified .. data.changed
---         hl_right = "DiffModified"
---       end
---       if data.removed ~= nil and data.removed > 0 then
---         s = s .. " %#StatusLineDiffRemoved#" .. icon.diff_removed .. data.removed
---         hl_right = "DiffRemoved"
---       end
---       s = s .. string.format("%%#StatusLine%sSep#%s", hl_right, separator.right)
---       return s
---     end
---   end
---   return ""
--- end
-
--- -- LSP status
--- local function diagnostic_info(inactive)
---   if inactive or #vim.lsp.buf_get_clients(0) == 0 then
---     return ""
---   end
---   local active_clients = vim.lsp.get_active_clients()
-
---   if active_clients then
---     local levels = {
---       error = "Error",
---       warn = "Warning",
---       info = "Information",
---       hint = "Hint",
---     }
---     local icons = { error = " ", warn = " ", info = " ", hint = " " }
---     local data = {}
---     local bufnr = vim.api.nvim_get_current_buf()
---     for k, level in pairs(levels) do
---       local count = 0
---       for _, client in ipairs(active_clients) do
---         count = count + vim.lsp.diagnostic.get_count(bufnr, level, client.id)
---       end
---       data[k] = count
---     end
-
---     local s = ""
---     local hl_left, hl_right
---     if data.error ~= nil and data.error > 0 then
---       if not hl_left then
---         hl_left = "DiagnosticError"
---       end
---       s = s .. string.format("%%#StatusLineDiagnosticError# %s %s ", icons.error, data.error)
---       hl_right = "DiagnosticError"
---     end
---     if data.warn ~= nil and data.warn > 0 then
---       if not hl_left then
---         hl_left = "DiagnosticWarning"
---       end
---       s = s .. string.format("%%#StatusLineDiagnosticWarning# %s %s ", icons.warn, data.warn)
---       hl_right = "DiagnosticWarning"
---     end
---     if data.info ~= nil and data.info > 0 then
---       if not hl_left then
---         hl_left = "DiagnosticInfo"
---       end
---       s = s .. string.format("%%#StatusLineDiagnosticInfo# %s %s ", icons.info, data.info)
---       hl_right = "DiagnosticInfo"
---     end
---     if data.hint ~= nil and data.hint > 0 then
---       if not hl_left then
---         hl_left = "DiagnosticHint"
---       end
---       s = s .. string.format("%%#StatusLineDiagnosticHint# %s %s ", icons.hint, data.hint)
---       hl_right = "DiagnosticHint"
---     end
---     if #s > 0 then
---       return string.format("%s%s%s", left_separator(hl_left), s, right_separator(hl_right))
---     end
---   end
---   return ""
--- end
-
--- -- [[ Utility functions ]]
-
--- -- Colors
--- local function highlight(group, guifg, guibg, attr)
---   local parts = { group }
---   if guifg then
---     table.insert(parts, "guifg=" .. guifg)
---   end
---   if guibg then
---     table.insert(parts, "guibg=" .. guibg)
---   end
---   if attr then
---     table.insert(parts, "gui=" .. attr)
---     table.insert(parts, "cterm=" .. attr)
---   end
---   -- if guisp then table.insert(parts, "guisp=#"..guisp) end
---   vim.cmd("highlight " .. table.concat(parts, " "))
--- end
-
--- local function apply_theme()
---   if type(M.theme) == "string" then
---     M.theme = require("appearance.themes." .. M.theme)
---   end
---   local base_bg = M.theme["Base"].bg
---   for group, colors in pairs(M.theme) do
---     highlight("StatusLine" .. group, colors.fg, colors.bg, colors.attr)
---     if group ~= "Base" then
---       highlight("StatusLine" .. group .. "Sep", colors.bg, base_bg, nil)
---     end
---   end
---   theme_set = M.theme
--- end
-
--- -- build statusline
--- local function make_statusline(inactive)
---   if M.theme ~= theme_set then
---     apply_theme()
---   end
---   inactive = inactive or false
---   local space = "%#StatusLineBase# "
---   local s = space
---   s = s .. vi_mode(inactive)
---   s = s .. space
---   s = s .. file_info(inactive)
---   s = s .. space
---   s = s .. git_info(inactive)
---   s = s .. space
---   s = s .. "%="
---   s = s .. diagnostic_info(inactive)
---   s = s .. space
---   if vim.api.nvim_win_get_width(0) > 80 then
---     s = s .. file_encoding_and_format(inactive)
---     s = s .. space
---   end
---   s = s .. line_colmun_info(inactive)
---   s = s .. space
---   return s
--- end
-
--- -- Create augroup for updating statusline
--- local function statusline_augroup()
---   _G.statusline_active = require("appearance.statusline").statusline_active
---   _G.statusline_inactive = require("appearance.statusline").statusline_inactive
-
---   vim.cmd [[
---     augroup statusline_autocmd
---     autocmd!
---     autocmd VimEnter,ColorScheme * lua require'appearance.statusline'.statusline_apply_theme()
---     autocmd WinLeave,BufLeave * setlocal statusline=%!v:lua.statusline_inactive()
---     autocmd BufWinEnter,WinEnter,BufEnter,TermOpen * setlocal statusline=%!v:lua.statusline_active()
---     autocmd VimResized,BufWritePost,FileChangedShellPost * redrawstatus
---     augroup END
---   ]]
--- end
-
--- -- [[ API ]]
--- function M.statusline_active()
---   return make_statusline(false)
--- end
-
--- function M.statusline_inactive()
---   return make_statusline(true)
--- end
-
--- M.statusline_apply_theme = apply_theme
-
--- function M.setup(theme_name)
---   M.theme = theme_name
---   statusline_augroup()
--- end
+local function diagnostics_hints()
+  return get_diagnostics("Warning", " ")
+end
 
 -- [ Building statusline ]
 local components_active = {
@@ -508,6 +247,17 @@ local components_active = {
     { provider = file_name, hl = "StatuslineFileName", left_sep = " ", right_sep = "" },
     { provider = file_modified, hl = "StatuslineFileModified", left_sep = " ", right_sep = "" },
     { provider = file_readonly, hl = "StatuslineFileReadonly", left_sep = " ", right_sep = "" },
+    { provider = git_branch, hl = "StatuslineGitBranch", left_sep = " ", right_sep = "" },
+    { provider = git_diff_added, hl = "StatuslineDiffAdded", left_sep = " ", right_sep = "" },
+    { provider = git_diff_removed, hl = "StatuslineDiffRemoved", left_sep = " ", right_sep = "" },
+    { provider = git_diff_changed, hl = "StatuslineDiffModified", left_sep = " ", right_sep = "" },
+  },
+  middle = {
+    { provider = lsp_client_names, hl = "StatuslineLspClient", left_sep = "", right_sep = "" },
+    { provider = diagnostics_errors, hl = "StatuslineDiagnosticError", left_sep = " ", right_sep = "" },
+    { provider = diagnostics_warnings, hl = "StatuslineDiagnosticWarning", left_sep = " ", right_sep = "" },
+    { provider = diagnostics_info, hl = "StatuslineDiagnosticInfo", left_sep = " ", right_sep = "" },
+    { provider = diagnostics_hints, hl = "StatuslineDiagnosticHint", left_sep = " ", right_sep = "" },
   },
   right = {
     { provider = file_size, hl = "StatuslineFileSize", left_sep = "", right_sep = " " },
@@ -554,21 +304,24 @@ local function set_statusline()
   )
 end
 
--- Entry point for setting statusline
+--- Set option and autocmds for statusline
 function M.setup()
   set_statusline()
 end
 
+--- Construct strings for the statusline
 function M.statusline()
-  local left, right
+  local left, middle, right
   if vim.g.statusline_winid == vim.fn.win_getid() then
     left = build_statusline(components_active.left)
+    middle = build_statusline(components_active.middle)
     right = build_statusline(components_active.right)
+    return left .. "%=" .. middle .. "%=" .. right
   else
     left = build_statusline(components_inactive.left)
     right = build_statusline(components_inactive.right)
+    return left .. "%=" .. right
   end
-  return left .. "%=" .. right
 end
 
 return M
--- editors/nvim/lua/rc/theme/nightfox.lua
+++ editors/nvim/lua/rc/theme/nightfox.lua
@@ -18,6 +18,7 @@ local opts = {
     StatuslineModeVisual = { fg = "${black}", bg = "${magenta}", style = "bold" },
     StatuslineModeReplace = { fg = "${black}", bg = "${red}", style = "bold" },
     StatuslineModeCommand = { fg = "${black}", bg = "${yellow}", style = "bold" },
+    StatuslineModeSelect = { fg = "${black}", bg = "${cyan}", style = "bold" },
     StatuslineModeOther = { fg = "${black}", bg = "${white}", style = "bold" },
     -- File info
     StatuslineFileIcon = { fg = "${blue}", bg = "${bg_statusline}" },
@@ -26,7 +27,7 @@ local opts = {
     StatuslineFileReadonly = { fg = "${red}", bg = "${bg_statusline}" },
     StatuslineFileSize = { fg = "${cyan}", bg = "${bg_statusline}" },
     -- Git
-    StatuslineGitBranch = { fg = "${purple}", bg = "${bg_statusline}" },
+    StatuslineGitBranch = { fg = "${blue_dm}", bg = "${bg_statusline}" },
     -- Diff
     StatuslineDiffAdded = { fg = "${git.add}", bg = "${bg_statusline}" },
     StatuslineDiffModified = { fg = "${git.change}", bg = "${bg_statusline}" },
@@ -37,7 +38,7 @@ local opts = {
     StatuslineDiagnosticError = { fg = "${error}", bg = "${bg_statusline}" },
     StatuslineDiagnosticWarning = { fg = "${warning}", bg = "${bg_statusline}" },
     StatuslineDiagnosticInfo = { fg = "${info}", bg = "${bg_statusline}" },
-    StatuslineDiagnosticHing = { fg = "${hint}", bg = "${bg_statusline}" },
+    StatuslineDiagnosticHint = { fg = "${hint}", bg = "${bg_statusline}" },
   },
 }
 if vim.fn.exists "g:GuiLoaded" == 0 and vim.fn.has "gui" == 0 and vim.fn.exists "$SSH_CONNECTION" == 0 then
@@ -46,3 +47,64 @@ end
 
 nightfox.setup(opts) -- configure
 nightfox.load() -- set colorscheme
+
+-- local colors = {
+--   meta = { name = "palefox", light = false },
+
+--   none = "NONE",
+--   bg = "#3d3f52",
+
+--   fg = "#f8f8f2",
+--   fg_gutter = "#5a5475",
+
+--   black = "#3b3a32",
+--   red = "#f1756f",
+--   green = "#6de874",
+--   yellow = "#f0e656",
+--   blue = "#a381ff",
+--   magenta = "#ff87b1",
+--   cyan = "#7ef5b8",
+--   white = "#c7b7c7",
+--   orange = "#f5b87f",
+--   pink = "#ffb8d1",
+
+--   -- +15 brightness, -15 saturation
+--   -- black_br = "#685868",
+--   black_br = "#9c97ac",
+--   red_br = "#ff9f9a",
+--   green_br = "#7bfa81",
+--   yellow_br = "#ffea00",
+--   blue_br = "#c5a3ff",
+--   magenta_br = "#ffb8d1",
+--   cyan_br = "#c2ffdf",
+--   white_br = "#ffefff",
+--   orange_br = "#efc39a",
+--   pink_br = "#fbc7d9",
+
+--   -- -15 brightness, -15 saturation
+--   black_dm = "#31302a",
+--   red_dm = "#df534c",
+--   green_dm = "#4dd554",
+--   yellow_dm = "#b4990f",
+--   blue_dm = "#7e55f0",
+--   magenta_dm = "#f1588e",
+--   cyan_dm = "#59e49c",
+--   white_dm = "#ac99ac",
+--   orange_dm = "#e49c59",
+--   pink_dm = "#f57fa9",
+
+--   comment = "#6f6b80",
+
+--   git = {
+--     add = "#70a288",
+--     change = "#a58155",
+--     delete = "#904a6a",
+--     conflict = "#c07a6d",
+--   },
+
+--   gitSigns = {
+--     add = "#164846",
+--     change = "#394b70",
+--     delete = "#823c41",
+--   },
+-- }
