85636876c9b230c32bfe7f3ea3017bac9b747a7f
Hidekazu Ikeno <hide.ikeno@gmail.com>
2022-02-09 18:47:45 +0900

    nvim: turn on the profiling in impatient.nvim

--- editors/nvim/init.lua
+++ editors/nvim/init.lua
@@ -1,8 +1,9 @@
 -- ~/.config/nvim/init.lua -- NeoVim configuration
--- local ok, impatient = pcall(require, "impatient")
--- if ok then
---   impatient.enable_profile()
--- end
-pcall(require, "impatient")
+local ok, impatient = pcall(require, "impatient")
+if ok then
+  impatient.enable_profile()
+else
+  vim.notify(impatient)
+end
 
 require "rc.bootstrap"
