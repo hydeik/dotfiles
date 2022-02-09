792d5e920419402d900b33fa4911bd37c0849785
Hidekazu Ikeno <hide.ikeno@gmail.com>
2022-02-09 16:44:48 +0900

    nvim: vim.lsp.diagnostic_* are deprecated. (update)

--- editors/nvim/lua/rc/config/lsp/keymap.lua
+++ editors/nvim/lua/rc/config/lsp/keymap.lua
@@ -13,18 +13,12 @@ local M = {}
 function M.setup(client, bufnr)
   vim.keymap.set("n", "ma", "<Cmd>lua vim.lsp.buf.code_action()<CR>", { buffer = bufnr })
   vim.keymap.set("n", "md", "<Cmd>lua vim.lsp.buf.definition()<CR>", { buffer = bufnr })
-  vim.keymap.set("n", "me", function()
-    vim.lsp.diagnostic.show_line_diagnostics { border = "single" }
-  end, { buffer = bufnr })
+  vim.keymap.set("n", "me", "<Cmd>lua vim.diagnostic.open_float()<CR>", { buffer = bufnr })
   vim.keymap.set("n", "mh", "<Cmd>lua vim.lsp.buf.hover()<CR>", { buffer = bufnr })
   vim.keymap.set("n", "mR", "<Cmd>lua vim.lsp.buf.rename()<CR>", { buffer = bufnr })
   vim.keymap.set("n", "m?", "<Cmd>lua vim.lsp.buf.signature_help()<CR>", { buffer = bufnr })
-  vim.keymap.set("n", "m[", function()
-    vim.lsp.diagnostic.goto_prev { popup_opts = { border = "single" } }
-  end, { buffer = bufnr })
-  vim.keymap.set("n", "m]", function()
-    vim.lsp.diagnostic.goto_next { popup_opts = { border = "single" } }
-  end, { buffer = bufnr })
+  vim.keymap.set("n", "m[", "<Cmd>lua vim.diagnostic.goto_prev()<CR>", { buffer = bufnr })
+  vim.keymap.set("n", "m]", "<Cmd>lua vim.diagnostic.goto_next()<CR>", { buffer = bufnr })
   vim.keymap.set("n", "mD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", { buffer = bufnr })
   vim.keymap.set("n", "mi", "<Cmd>lua vim.lsp.buf.type_definition()<CR>", { buffer = bufnr })
   vim.keymap.set("n", "mr", "<Cmd>lua vim.lsp.buf.references()<CR>", { buffer = bufnr })
