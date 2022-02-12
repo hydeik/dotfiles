9578a236d77fcc16b2a879fdd41332a00b06720d
Hidekazu Ikeno <hide.ikeno@gmail.com>
2022-02-12 20:41:45 +0900

    update poetry install script

--- run_once_install-poetry.sh
+++ run_once_install-poetry.sh
@@ -7,5 +7,5 @@ POETRY_DIR="$HOME/.poetry"
 if [ -x "$POETRY_DIR/bin/poetry" ]; then
   echo "Poetry is already installed"
 else
-  curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+  curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | python -
 fi
