9a64194765f28a2951df1ecc9b2fb602244b14a5
Hidekazu Ikeno <hide.ikeno@gmail.com>
2022-05-17 00:58:42 +0900

    nvim:update

--- private_dot_config/nvim/lua/rc/config/iswap.lua
+++ private_dot_config/nvim/lua/rc/config/iswap.lua
@@ -1,7 +1,8 @@
 local M = {}
 
 function M.config()
-  vim.keymap.set("n", "<Leader>s", "<Cmd>ISwapWith<CR>", { silent = true })
+  vim.keymap.set("n", "<Leader>s", "<Cmd>ISwap<CR>", { silent = true })
+  vim.keymap.set("n", "<Leader>S", "<Cmd>ISwapWith<CR>", { silent = true })
 end
 
 return M
