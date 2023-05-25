4b08e770f145c550c4b81cd544b56f47dc303123
Hidekazu Ikeno <hide.ikeno@gmail.com>
2023-05-25 17:18:02 +0900

    zsh: cache the completion for verdi (AiiDa)

--- private_dot_config/zsh/rc/50_completions.zsh
+++ private_dot_config/zsh/rc/50_completions.zsh
@@ -57,3 +57,42 @@ _python_argcomplete() {
 if (( ${+commands[pipx]} )); then
     complete -o nospace -o default -o bashdefault -F _python_argcomplete pipx
 fi
+
+##
+## Caching the result of (_VERDI_COMPLETE=zsh_source verdi)
+##
+#compdef verdi
+
+_verdi_completion() {
+    local -a completions
+    local -a completions_with_descriptions
+    local -a response
+    (( ! $+commands[verdi] )) && return 1
+
+    response=("${(@f)$(env COMP_WORDS="${words[*]}" COMP_CWORD=$((CURRENT-1)) _VERDI_COMPLETE=zsh_complete verdi)}")
+
+    for type key descr in ${response}; do
+        if [[ "$type" == "plain" ]]; then
+            if [[ "$descr" == "_" ]]; then
+                completions+=("$key")
+            else
+                completions_with_descriptions+=("$key":"$descr")
+            fi
+        elif [[ "$type" == "dir" ]]; then
+            _path_files -/
+        elif [[ "$type" == "file" ]]; then
+            _path_files -f
+        fi
+    done
+
+    if [ -n "$completions_with_descriptions" ]; then
+        _describe -V unsorted completions_with_descriptions -U
+    fi
+
+    if [ -n "$completions" ]; then
+        compadd -U -V unsorted -a completions
+    fi
+}
+
+compdef _verdi_completion verdi;
+
