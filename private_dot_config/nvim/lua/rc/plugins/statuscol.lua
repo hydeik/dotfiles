fb48e8fb9e6f153ce47d0580fb2e8b6394bee11f
Hidekazu Ikeno <hide.ikeno@gmail.com>
2023-07-25 17:25:27 +0900

    nvim: remove configs for unused plugins

--- private_dot_config/nvim/lua/rc/plugins/statuscol.lua
+++
@@ -1,9 +0,0 @@
--- Status column plugin that provides a configurable 'statuscolumn' and click handlers.
-return {
-  "luukvbaal/statuscol.nvim",
-  event = "VeryLazy",
-  enabled = false,
-  opts = {
-    relculright = true,
-  },
-}
