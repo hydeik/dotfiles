36382848c78ef2887103b1eaeb3aac397aec3b53
Hidekazu Ikeno <hide.ikeno@gmail.com>
2023-10-28 14:26:27 +0900

    remove asdf install script

--- run_once_install-asdf.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euxo pipefail
-
-if [ -f "$HOME/.asdf/asdf.sh" ]; then
-  echo "asdf is already installed"
-else
-  git clone https://github.com/asdf-vm/asdf.git "$HOME/.asdf" --branch v0.11.3
-fi
