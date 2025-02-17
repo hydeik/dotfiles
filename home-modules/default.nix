8c80c93943a01d36e3cdcd361d88e44ccf37edb2
Hidekazu Ikeno <hide.ikeno@gmail.com>
2025-02-17 18:12:43 +0900

    (wip) update

--- home-modules/default.nix
+++ home-modules/default.nix
@@ -1,5 +1,12 @@
 { ezModules, lib, inputs, ... }:
 {
+  programs.home-manager.enable = true;
+
+  home = {
+    stateVersion = "24.11";
+    preferXdgDirectories = true;
+  };
+
   imports = lib.attrValues
     {
       inherit(ezModules)
