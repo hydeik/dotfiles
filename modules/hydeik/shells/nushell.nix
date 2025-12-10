8df7d298974092a2458758b66f1c690bc0f3ce9e
Hidekazu Ikeno <hide.ikeno@gmail.com>
2025-12-10 15:07:13 +0900

    nix: add nushell packages and plugins

--- modules/hydeik/shells/nushell.nix
+++ modules/hydeik/shells/nushell.nix
@@ -1,9 +1,50 @@
+{ lib, ... }:
 {
   hydeik.shells.homeManager =
     { pkgs, ... }:
+    let
+      inherit (lib.strings) concatStringsSep;
+      inherit (pkgs.stdenvNoCC) isLinux;
+
+      pluginNamesNixpkgs = [
+        "formats"
+        "hcl"
+        "highlight"
+        "gstat"
+        # "net" # broken
+        "polars"
+        "query"
+        "semver"
+        "skim"
+        # "units" # broken
+      ]
+      ++ pkgs.lib.optionals isLinux [
+        "dbus"
+        "desktop_notifications"
+      ];
+
+      activateNushellPluginsNuScript = pkgs.writeTextFile {
+        name = "activateNushellPlugins";
+        destination = "/bin/activateNushellPlugins.nu";
+        text = ''
+          #!/usr/bin/env nu
+          ${concatStringsSep "\n" (
+            map (x: "plugin add ${pkgs.nushellPlugins.${x}}/bin/nu_plugin_${x}") pluginNamesNixpkgs
+          )}
+        '';
+      };
+
+      msgPackz = pkgs.runCommand "nushellMsgPackz" { } ''
+        mkdir -p "$out"
+        # After some experimentation, I determined that this only works if --plugin-config is FIRST
+        ${pkgs.nushell}/bin/nu --plugin-config "$out/plugin.msgpackz" ${activateNushellPluginsNuScript}/bin/activateNushellPlugins.nu
+      '';
+    in
     {
       home.packages = with pkgs; [
         nushell
+        bash-env-json
+        nu_scripts
       ];
 
       programs.bash.profileExtra = ''
@@ -11,5 +52,7 @@
           exec nu
         fi
       '';
+
+      xdg.configFile."nushell/plugin.msgPackz".source = "${msgPackz}/plugin.msgpackz";
     };
 }
