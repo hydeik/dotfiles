e81c2827e4c0645fa09e812e4ad5687c6fb3aef8
Hidekazu Ikeno <hide.ikeno@gmail.com>
2026-01-28 13:54:54 +0900

    fix: nvim

--- dots/config/nvim/lua/rc/plugins/jujutsu.lua
+++ dots/config/nvim/lua/rc/plugins/jujutsu.lua
@@ -31,6 +31,18 @@ return {
   },
   {
     "MrDwarf7/lazyjui.nvim",
+    opts = {
+      -- Optionally (default):
+      border = {
+        -- Use custom set of border chars (must be 8 long)
+        -- chars = { "", "", "", "", "", "", "", "" }, -- either set all to empty to remove the entire outer border (or nil/{})
+        chars = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+        thickness = 0, -- This handles the border of the 'outer' window it's nested inside, generally this is invisible
+        -- See `:h nvim_win_set_hl_ns()` and associated docs for more details
+        -- previous option was: "FloatBorder:LazyJuiBorder,NormalFloat:LazyJuiFloat", -- up to you how to set
+        winhl_str = "",
+      },
+    },
     keys = {
       {
         "<Space>gj",
