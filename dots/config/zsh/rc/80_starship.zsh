4c381ac96663d3000e55af611ec91a08d76d8fc5
Hidekazu Ikeno <hide.ikeno@gmail.com>
2026-01-28 21:58:38 +0900

    zsh: adjust starship exec pathj

--- dots/config/zsh/rc/80_starship.zsh
+++ dots/config/zsh/rc/80_starship.zsh
@@ -9,11 +9,12 @@
 
 zmodload zsh/parameter  # Needed to access jobstates variable for STARSHIP_JOBS_COUNT
 
+STARSHIP_BIN=$HOME/.nix-profile/bin/starship
 # Defines a function `__starship_get_time` that sets the time since epoch in millis in STARSHIP_CAPTURED_TIME.
 if [[ $ZSH_VERSION == ([1-4]*) ]]; then
     # ZSH <= 5; Does not have a built-in variable so we will rely on Starship's inbuilt time function.
     __starship_get_time() {
-        STARSHIP_CAPTURED_TIME=$(/Users/ikeno/.nix-profile/bin/starship time)
+        STARSHIP_CAPTURED_TIME=$(${STARSHIP_BIN} time)
     }
 else
     zmodload zsh/datetime
@@ -97,6 +98,6 @@ VIRTUAL_ENV_DISABLE_PROMPT=1
 
 setopt promptsubst
 
-PROMPT='$('/Users/ikeno/.nix-profile/bin/starship' prompt --terminal-width="$COLUMNS" --keymap="${KEYMAP:-}" --status="${STARSHIP_CMD_STATUS:-}" --pipestatus="${STARSHIP_PIPE_STATUS[*]:-}" --cmd-duration="${STARSHIP_DURATION:-}" --jobs="$STARSHIP_JOBS_COUNT")'
-RPROMPT='$('/Users/ikeno/.nix-profile/bin/starship' prompt --right --terminal-width="$COLUMNS" --keymap="${KEYMAP:-}" --status="${STARSHIP_CMD_STATUS:-}" --pipestatus="${STARSHIP_PIPE_STATUS[*]:-}" --cmd-duration="${STARSHIP_DURATION:-}" --jobs="$STARSHIP_JOBS_COUNT")'
-PROMPT2="$(/Users/ikeno/.nix-profile/bin/starship prompt --continuation)"
+PROMPT='$("$STARSHIP_BIN" prompt --terminal-width="$COLUMNS" --keymap="${KEYMAP:-}" --status="${STARSHIP_CMD_STATUS:-}" --pipestatus="${STARSHIP_PIPE_STATUS[*]:-}" --cmd-duration="${STARSHIP_DURATION:-}" --jobs="$STARSHIP_JOBS_COUNT")'
+RPROMPT='$("$STARSHIP_BIN" prompt --right --terminal-width="$COLUMNS" --keymap="${KEYMAP:-}" --status="${STARSHIP_CMD_STATUS:-}" --pipestatus="${STARSHIP_PIPE_STATUS[*]:-}" --cmd-duration="${STARSHIP_DURATION:-}" --jobs="$STARSHIP_JOBS_COUNT")'
+PROMPT2='$("$STARSHIP_BIN" prompt --continuation)'
